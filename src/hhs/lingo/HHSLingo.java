fd8e1ae4c7d8549646574da3380191502732a610
DeStilleGast <DeStilleGast@hotmail.com>
2018-10-04 09:04:00 +0200

    Adding IDE files

---
+++ src/hhs/lingo/HHSLingo.java
@@ -0,0 +1,17 @@
+
+package hhs.lingo;
+
+/**
+ *
+ * @author Falco Strijk
+ */
+public class HHSLingo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
