042c49c3625da03e21bac5b635aaad4800cb0224
DeStilleGast <DeStilleGast@hotmail.com>
2018-10-05 13:30:29 +0200

    See last commit message

--- src/hhs/lingo/HHSLingo.java
+++ src/hhs/lingo/HHSLingo.java
@@ -1,17 +1,74 @@
 
 package hhs.lingo;
 
+import javax.swing.*;
+
 /**
  *
- * @author Falco Strijk
+ * @author Falco
  */
 public class HHSLingo {
+
+    private int pogingen = 0;
+    private final int maxAantalPogingen = 5;
+
+    private JTextField[] textFields;
+    private boolean isSpelActief;
+    
+    private String hetWoord;
+
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new HHSLingo().run();
     }
 
+    public void run(){
+        // Alles opzetten
+    }
+
+    public void StartSpel(){
+        isSpelActief = true;
+    }
+
+    // Hier woord het woord uit een array gehaalt en dat woord moet geraden worden
+    public String VerkrijgNieuwWoord(){ 
+
+        return "";
+    }
+    
+    // Hier zit de logica dat de gui invult
+    public void VulInWoord(int poging, String woord){ 
+
+    }
+
+    public boolean IsCheat(String input){
+        char start = input.charAt(0);
+        int teller = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == start) {
+                teller++;
+            }
+        }
+
+        return teller == input.length();
+    }
+
+    // Als er vals gespeelt wordt, wat moet er gedaan worden
+    public void EindSpelValsSpel(){ 
+
+    }
+
+    // Als de speler gewonnen heeft
+    public void EindSpelWin(){ 
+
+    }
+    
+    // Als de speler geen kansen meer heeft
+    public void EindSpelGeenKansenOver(){ 
+
+    }
 }
